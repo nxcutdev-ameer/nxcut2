1812de098e8aef985f9d71878c536d82d567095b
macbookpro <macbookpro@macbookpros-MacBook-Pro.local>
2025-11-17 17:05:05 +0400

    modified app.json.js

--- app.json.js
+++ app.json.js
@@ -8,36 +8,39 @@ export default {
     splash: {
       image: "./assets/splash.png",
       resizeMode: "contain",
-      backgroundColor: "#ffffff"
+      backgroundColor: "#ffffff",
     },
     userInterfaceStyle: "light",
     ios: {
       bundleIdentifier: "com.qdealsllc.nxcut",
-      buildNumber: "9"
+      buildNumber: "9",
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false,
+      },
     },
     android: {
       package: "com.qdealsllc.nxcut",
       versionCode: 9,
       adaptiveIcon: {
         foregroundImage: "./assets/icon.png",
-        backgroundColor: "#ffffff"
-      }
+        backgroundColor: "#ffffff",
+      },
     },
-    plugins: [
-      "expo-splash-screen",
-      "expo-system-ui"
-    ],
+    plugins: ["expo-splash-screen", "expo-system-ui"],
     updates: {
-      url: "https://u.expo.dev/YOUR-UPDATE-ID"
+      url: "https://u.expo.dev/YOUR-UPDATE-ID",
     },
     runtimeVersion: {
-      policy: "sdkVersion"
+      policy: "sdkVersion",
     },
     extra: {
       supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
       supabaseKey: process.env.EXPO_PUBLIC_SUPABASE_KEY,
       supabaseGraphqlUrl: process.env.EXPO_PUBLIC_SUPABASE_GRAPHQL_URL,
-      baseUrl: process.env.BASE_URL
-    }
-  }
+      baseUrl: process.env.BASE_URL,
+      eas: {
+        projectId: "a70fb7da-69f6-4497-b893-c89cfe1371da",
+      },
+    },
+  },
 };
